ba26a14f7d7b5fc041fc10a7c80a43d648c0cdee
CrypticDesign <mashoobee@gmail.com>
2018-05-16 20:41:27 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,8 +4,6 @@ const client = new Discord.Client();
 client.on('ready', () => {
     console.log('I am ready!');
     
-    bot.user.setStatus('Idle');
-    
 });
 
 client.on('message', message => {
@@ -13,6 +11,30 @@ client.on('message', message => {
     	message.reply('pong');
   	}
 });
+
+const cheerio = require('cheerio')
+	, snekfetch = require('snekfetch')
+	, querystring = require('querystring');
+
+async function googleCommand(msg, args) {
+
+	let searchMessage = await < Message > .reply('Searching... Sec.');
+	let searchUrl = `https://www.google.com/search?q=${encodeURIComponent(msg.content)}`;
+	return snekfetch.get(searchUrl)
+		.then((result) => {
+			let $ = cheerio.load(result.text);
+			let googleData = $('.r')
+				.first()
+				.find('a')
+				.first()
+				.attr('href');
+			googleData = querystring.parse(googleData.replace('/url?', ''));
+			searchMessage.edit(`Result found!\n${googleData.q}`);
+		});
+  
+		.catch((err) => {
+			searchMessage.edit('No results found!');
+		});
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
