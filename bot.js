3fa4ae2fa761a81139a02262d5954f34a553ddff
CrypticDesign <mashoobee@gmail.com>
2018-05-17 21:40:21 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.content === process.env.PREFIX + 'ping') {
+    if (message.content === 'ping') {
     	message.reply('pong');
   	}
 });
