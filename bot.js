4c623c6e81669912e44dcb120a1f07d510807dc7
CrypticDesign <mashoobee@gmail.com>
2018-05-18 19:51:21 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const prefix = "b.";
+const prefix = "b!";
 
 client.on('ready', () => {
     console.log('I am ready!');
